fa0c3ad1aae38255306f096b7bf7b11e8d24025f
Caleb-Kronstad <calebtkronstad@gmail.com>
2026-02-15 18:45:52 -0500

    assignment 2

--- Assignment2/cashier.py
+++ Assignment2/cashier.py
@@ -3,11 +3,20 @@ class Cashier:
         pass
 
     def process_coins(self):
-        """Returns the total calculated from coins inserted.
-           Hint: include input() function here, e.g. input("how many quarters?: ")"""
-        ###
+        print("Please insert coins.")
+        quarters = int(input("how many quarters?: "))
+        dimes = int(input("how many dimes?: "))
+        nickels = int(input("how many nickels?: "))
+        pennies = int(input("how many pennies?: "))
+        total = quarters * 0.25 + dimes * 0.10 + nickels * 0.05 + pennies * 0.01
+        return total
 
     def transaction_result(self, coins, cost):
-        """Return True when the payment is accepted, or False if money is insufficient.
-           Hint: use the output of process_coins() function for cost input"""
-        ##
+        if coins >= cost:
+            change = round(coins - cost, 2)
+            if change > 0:
+                print("Here is $" + str(change) + " in change.")
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            return False
